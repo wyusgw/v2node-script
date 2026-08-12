882695e5271ebb95d9fe97fae55d093add8f6580
wyusgw <wyusgw@gmail.com>
2026-08-12 14:05:21 +0800

    first commit

--- script/install.sh
+++ script/install.sh
@@ -375,7 +375,7 @@ EOF
     fi
 
 
-    curl -o /usr/bin/v2node -Ls https://raw.githubusercontent.com/wyusgw/v2node/main/script/v2node.sh
+    curl -o /usr/bin/v2node -Ls https://raw.githubusercontent.com/wyusgw/v2node-script/script/v2node.sh
     chmod +x /usr/bin/v2node
 
     cd $cur_dir

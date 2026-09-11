9e292fa3de580ddf9bc4fc7eca0c086c2c9319ae
wyusgw <wyusgw@gmail.com>
2026-09-11 15:09:40 +0800

    Remove delete WireGuard

--- script/install.sh
+++ script/install.sh
@@ -55,7 +55,7 @@ parse_args() {
                 API_KEY_ARG="$2"; shift 2 ;;
             -h|--help)
                 echo "用法: $0 [版本号] [--api-host URL] [--node-id ID] [--node-type TYPE] [--api-key KEY]"
-                echo "节点类型: vmess / vless / trojan / shadowsocks / hysteria2 / tuic / anytls / mieru / wireguard"
+                echo "节点类型: vmess / vless / trojan / shadowsocks / hysteria2 / tuic / anytls / mieru"
                 exit 0 ;;
             --*)
                 echo "未知参数: $1"; exit 1 ;;
